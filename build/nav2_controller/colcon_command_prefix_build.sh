10eb20af8818d4ffbf6efb3cc613071c2109a704
wenbo <1878087979@qq.com>
2025-07-01 06:14:05 +0800

    test

---
+++ build/nav2_controller/colcon_command_prefix_build.sh
@@ -0,0 +1,11 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/wb/ros_ws/install/nav2_common/share/nav2_common/package.sh"
+. "/home/wb/ros_ws/install/nav_2d_msgs/share/nav_2d_msgs/package.sh"
+. "/home/wb/ros_ws/install/nav2_msgs/share/nav2_msgs/package.sh"
+. "/home/wb/ros_ws/install/nav2_voxel_grid/share/nav2_voxel_grid/package.sh"
+. "/home/wb/ros_ws/install/nav2_util/share/nav2_util/package.sh"
+. "/home/wb/ros_ws/install/nav2_lifecycle_manager/share/nav2_lifecycle_manager/package.sh"
+. "/home/wb/ros_ws/install/nav2_map_server/share/nav2_map_server/package.sh"
+. "/home/wb/ros_ws/install/nav_2d_utils/share/nav_2d_utils/package.sh"
+. "/home/wb/ros_ws/install/nav2_costmap_2d/share/nav2_costmap_2d/package.sh"
+. "/home/wb/ros_ws/install/nav2_core/share/nav2_core/package.sh"
---
+++ build/nav2_simple_commander/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/wb/ros_ws/install/nav2_simple_commander'
